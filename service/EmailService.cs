68ad0584bc30cf6a8e478e9c2c5ce6580891b38f
goruchie <marcosgualtero43253@gmail.com>
2024-12-04 20:21:59 -0300

    Implement the Service structure for Sign up and Log in features.

---
+++ service/EmailService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace service
+{
+    public class EmailService
+    {
+        private MailMessage email;
+        private SmtpClient server;
+        public EmailService() 
+        {
+            server = new SmtpClient();
+            server.Credentials = new NetworkCredential("c83ab849b588a5", "********4b03");
+            server.EnableSsl = true;
+            server.Port = 2525;
+            server.Host = "sandbox.smtp.mailtrap.io";
+        }
+        public void setEmail(string mail, string subject, string body)
+        {
+            email = new MailMessage();
+            email.From = new MailAddress("marcosgualtero@hotmail.com");
+            email.To.Add(mail);
+            email.Subject = subject;
+            email.IsBodyHtml = true;
+            email.Body = body;
+        }
+        public void sendEmail()
+        {
+            try
+            {
+                server.Send(email);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
---
+++ service/TraineeService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using domain;
+
+namespace service
+{
+    public class TraineeService
+    {
+        public int insertNew(Trainee newTrainee)
+        {           
+            DataAccess data = new DataAccess();
+            try
+            {
+                data.setProcedure("insertNew");
+                data.setParameter("@email", newTrainee.Email);
+                data.setParameter("@pass", newTrainee.Pass);
+                return data.executeScalar();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                data.closeConnection();
+            }
+        }
+    }
+}
