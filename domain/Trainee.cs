6a1abe131347d9c8f0c200ef3c39cf95dbd258a9
goruchie <marcosgualtero43253@gmail.com>
2024-12-04 19:30:26 -0300

    Implement the Trainee class.

---
+++ domain/Trainee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace domain
+{
+    public class Trainee
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Pass { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime Date { get; set; }
+        public bool Admin { get; set; }
+        public string ProfileImage { get; set; }
+
+    }
+}
